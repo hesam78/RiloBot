d7e32cb94b65fd1ce3ec3046fc44acd1ca0092b1
hesam78 <h.onlineboy1@gmail.com>
2016-01-30 20:05:36 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -225,7 +225,7 @@ function create_config( )
     "all",
     "leave_ban",
     "admin"
-    },
+   sudo_users={151357909},sudo--sudo users
     sudo_users = {110626080,103649648,111020322,111020322,0,tonumber(our_id)},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
